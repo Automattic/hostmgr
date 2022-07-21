1dcf573bd4db554262f6c89f7f2f15832a65cce8
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2022-07-21 14:31:42 -0600

    Address PR Feedback

--- Sources/hostmgr/commands/vm/VMList.swift
+++ Sources/hostmgr/commands/vm/VMList.swift
@@ -40,58 +40,58 @@ struct VMListCommand: ParsableCommand {
 
     private func printRunningVMs() throws {
         try Parallels().lookupRunningVMs().forEach {
-            printStatus(status: .running, virtualMachine: $0, ipAddress: $0.ipAddress)
+            print(status: .running, virtualMachine: $0, ipAddress: $0.ipAddress)
         }
     }
 
     private func printStoppedVMs() throws {
         try Parallels().lookupStoppedVMs().forEach {
-            printStatus(status: .stopped, virtualMachine: $0)
+            print(status: .stopped, virtualMachine: $0)
         }
     }
 
     private func printSuspendedVMs() throws {
         try Parallels().lookupSuspendedVMs().forEach {
-            printStatus(status: .suspended, virtualMachine: $0)
+            print(status: .suspended, virtualMachine: $0)
         }
     }
 
     private func printPackagedVMs() throws {
         try Parallels().lookupPackagedVMs().forEach {
-            printStatus(status: .packaged, virtualMachine: $0)
+            print(status: .packaged, virtualMachine: $0)
         }
     }
 
     private func printInvalidVMs() throws {
         try Parallels().lookupInvalidVMs().forEach {
-            printStatus(status: .invalid, virtualMachine: $0)
+            print(status: .invalid, virtualMachine: $0)
         }
     }
 
     private func printStartingVMs() throws {
         try Parallels().lookupStartingVMs().forEach {
-            printStatus(status: .starting, virtualMachine: $0)
+            print(status: .starting, virtualMachine: $0)
         }
     }
 
     private func printStoppingVMs() throws {
         try Parallels().lookupStoppingVMs().forEach {
-            printStatus(status: .stopping, virtualMachine: $0)
+            print(status: .stopping, virtualMachine: $0)
         }
     }
 
     private func printResumingVMs() throws {
         try Parallels().lookupStoppingVMs().forEach {
-            printStatus(status: .resuming, virtualMachine: $0)
+            print(status: .resuming, virtualMachine: $0)
         }
     }
 
-    private func printStatus(
+    private func print(
         status: VMStatus,
         virtualMachine: VMProtocol,
         ipAddress: String? = nil
     ) {
-        print("\(status.rawValue)\t\(virtualMachine.name)\t\(virtualMachine.uuid)")
+        Swift.print("\(status.rawValue)\t\(virtualMachine.name)\t\(virtualMachine.uuid)")
     }
 }
 
