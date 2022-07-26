17e20f180f813e05197b1fabe134e42e275da880
Tony Li <tony.li@automattic.com>
2022-07-27 11:24:33 +1200

    Simplify buildkite job script generation

--- Sources/hostmgr/commands/generate/GenerateBuildkiteJobScript.swift
+++ Sources/hostmgr/commands/generate/GenerateBuildkiteJobScript.swift
@@ -17,7 +17,7 @@ struct GenerateBuildkiteJobScript: ParsableCommand {
 
     func generateBuildScript() throws -> String {
         let exports = try generateExports()
-            .map { "export \($0.key)=\"\($0.value)\"" }
+            .map { "export \($0.key)=\"\($0.value.escapingQuotes())\"" }
             .joined(separator: "\n")
 
         return [
@@ -31,52 +31,7 @@ struct GenerateBuildkiteJobScript: ParsableCommand {
     func generateExports(
         from environment: [String: String] = ProcessInfo.processInfo.environment
     ) throws -> [String: String] {
-
-        // See https://buildkite.com/docs/pipelines/environment-variables for
-        // and up-to-date list of environment variables that Buildkite exports
-        let copyableExports = [
-            "BUILDKITE",
-            "BUILDKITE_AGENT_DEBUG",
-            "BUILDKITE_AGENT_DEBUG_HTTP",
-            "BUILDKITE_AGENT_ENDPOINT",
-            "BUILDKITE_AGENT_INCLUDE_RETRIED_JOBS",
-            "BUILDKITE_AGENT_META_DATA_QUEUE",
-            "BUILDKITE_AGENT_NAME",
-            "BUILDKITE_AGENT_NO_COLOR",
-            "BUILDKITE_AGENT_PROFILE",
-            "BUILDKITE_ARTIFACT_PATHS",
-            "BUILDKITE_ARTIFACT_UPLOAD_DESTINATION",
-            "BUILDKITE_BOOTSTRAP_PHASES",
-            "BUILDKITE_BRANCH",
-            "BUILDKITE_BUILD_NUMBER",
-            "BUILDKITE_BUILD_URL",
-            "BUILDKITE_CLEAN_CHECKOUT",
-            "BUILDKITE_COMMIT",
-            "BUILDKITE_GIT_CLONE_FLAGS",
-            "BUILDKITE_HOOKS_PATH",
-            "BUILDKITE_JOB_ID",
-            "BUILDKITE_LABEL",
-            "BUILDKITE_MESSAGE",
-            "BUILDKITE_NO_HTTP2",
-            "BUILDKITE_ORGANIZATION_SLUG",
-            "BUILDKITE_PIPELINE_PROVIDER",
-            "BUILDKITE_PIPELINE_SLUG",
-            "BUILDKITE_PULL_REQUEST",
-            "BUILDKITE_PULL_REQUEST_REPO",
-            "BUILDKITE_REFSPEC",
-            "BUILDKITE_REPO",
-            "BUILDKITE_TAG",
-
-            // These ones aren't printed as part of the default list – we're copying them so that `bootstrap` works
-            "BUILDKITE_AGENT_ACCESS_TOKEN",
-            "BUILDKITE_BUILD_ID",
-            "BUILDKITE_PLUGINS_PATH"
-
-        ].reduce([String: String]()) { dictionary, key in
-            var mutableDictionary = dictionary
-            mutableDictionary[key] = environment[key]
-            return mutableDictionary
-        }
+        let copyableExports = environment.filter { $0.key.starts(with: "BUILDKITE") }
 
         return [
             // Manually specify the build path to keep them nice and clean in the output
@@ -87,19 +42,6 @@ struct GenerateBuildkiteJobScript: ParsableCommand {
 
             // Required to convince `fastlane` that we're running in CI
             "CI": "true",
-
-            // We need to escape double slashes in the command / script, otherwise we can't pass it via command line
-            "BUILDKITE_COMMAND": environment["BUILDKITE_COMMAND"]?.escapingQuotes(),
-            "BUILDKITE_SCRIPT_PATH": environment["BUILDKITE_SCRIPT_PATH"]?.escapingQuotes(),
-
-            // We need to escape double slashes in the Buildkite Plugins JSON,
-            // otherwise it causes an inscrutable error like:
-            //
-            // ```
-            // Error: Failed to parse a plugin definition: invalid character 'g'
-            // looking for beginning of object key string
-            // ```
-            "BUILDKITE_PLUGINS": environment["BUILDKITE_PLUGINS"]?.escapingQuotes(),
 
             // Used by the S3 Git Mirror plugin
             "GIT_MIRROR_SERVER_ROOT": "http://\(try getIpAddress()):\( Configuration.shared.gitMirrorPort)"
