522372fb381d490b7c50013d9d41251bfbfa9823
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2022-10-25 17:53:30 -0600

    Allow overriding the log level via the environment

--- Sources/hostmgr/helpers/Logging.swift
+++ Sources/hostmgr/helpers/Logging.swift
@@ -21,8 +21,12 @@ extension Logger {
     static var shared = Logger(label: "com.automattic.hostmgr")
 
     static func initializeLoggingSystem() {
+
         #if DEBUG
         Logger.shared.logLevel = .trace
+        #else
+        let logLevelFromEnv = ProcessInfo.processInfo.environment["LOG_LEVEL"].flatMap { Logger.Level(rawValue: $0) }
+        Logger.shared.logLevel = logLevelFromEnv ?? .info
         #endif
 
         LoggingSystem.bootstrap { label in
