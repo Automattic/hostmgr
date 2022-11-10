bd992100536aa8dc3dc4dbbe0b30b0fa6ccc2a67
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2022-11-09 17:11:49 -0700

    Add File Transfer Progress Model

--- Sources/libhostmgr/S3Manager.swift
+++ Sources/libhostmgr/S3Manager.swift
@@ -63,9 +63,13 @@ public struct S3Manager: S3ManagerProtocol {
             return (FileManager.default.temporaryFilePath(), [.createIntermediateDirectories, .removePreviousFile])
         }
 
+        let startDate = Date()
+
         let temporaryFile = try await AF
             .download(signedURL, method: .get, to: destinationResolver)
-            .downloadProgress { progressCallback?(.progressData(from: $0)) }
+            .downloadProgress {
+                progressCallback?(.progressData(from: $0, withStartDate: startDate))
+            }
             .serializingDownloadedFileURL(automaticallyCancelling: true)
             .value
 
@@ -172,69 +176,5 @@ public struct S3Object {
     public init(key: String, size: Int) {
         self.key = key
         self.size = size
-    }
-}
-
-public struct FileTransferProgress {
-    public typealias Percentage = Decimal
-
-    let current: Int
-    let total: Int
-
-    public init(current: Int, total: Int) {
-        self.current = current
-        self.total = total
-    }
-
-    public var percent: Percentage {
-        Decimal(Double(self.current) / Double(self.total) * 100.0)
-    }
-
-    public var downloadedData: String {
-        ByteCountFormatter.string(fromByteCount: Int64(current), countStyle: .file)
-    }
-
-    public var totalData: String {
-        ByteCountFormatter.string(fromByteCount: Int64(total), countStyle: .file)
-    }
-
-    public func dataRate(timeIntervalSinceStart interval: TimeInterval) -> String {
-        let bytesPerSecond = Double(current) / interval
-
-        // Don't continue unless the rate can be represented by `Int64`
-        guard bytesPerSecond.isNormal else {
-            return ByteCountFormatter.string(fromByteCount: 0, countStyle: .file)
-        }
-
-        return ByteCountFormatter.string(fromByteCount: Int64(bytesPerSecond), countStyle: .file)
-    }
-
-    public func estimatedTimeRemaining(timeIntervalSinceStart interval: TimeInterval) -> TimeInterval {
-        let bytesPerSecond = Double(current) / interval
-
-        // Don't continue unless the rate makes some kind of sense
-        guard bytesPerSecond.isNormal else {
-            return .infinity
-        }
-
-        let totalNumberOfSeconds = Double(total) / bytesPerSecond
-
-        return totalNumberOfSeconds - interval
-    }
-
-    public var formattedPercentage: String {
-        let formatter = NumberFormatter()
-        formatter.alwaysShowsDecimalSeparator = true
-        formatter.roundingMode = .down
-        formatter.minimumFractionDigits = 2
-        formatter.maximumFractionDigits = 2
-        return formatter.string(from: percent as NSDecimalNumber)!
-    }
-
-    static func progressData(from progress: Progress) -> FileTransferProgress {
-        return FileTransferProgress(
-            current: Int(progress.completedUnitCount),
-            total: Int(progress.totalUnitCount)
-        )
     }
 }
