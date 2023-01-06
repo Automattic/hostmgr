da8d39cc5db27a4e5513009a476f1c8f80aabef5
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2023-01-06 16:03:18 -0700

    Address feedback

--- Sources/libhostmgr/Model/ConfigurationRepository.swift
+++ Sources/libhostmgr/Model/ConfigurationRepository.swift
@@ -6,19 +6,29 @@ public struct ConfigurationRepository {
         FileManager.default.fileExists(at: Paths.configurationFilePath)
     }
 
-    public static func getConfiguration() throws -> Configuration {
+    public static func createConfigurationDirectoryIfNeeded() throws {
         try FileManager.default.createDirectory(at: Paths.configurationRoot, withIntermediateDirectories: true)
+    }
+
+    public static func getConfiguration() throws -> Configuration {
+        try createConfigurationDirectoryIfNeeded()
         let data = try Data(contentsOf: Paths.configurationFilePath)
         return try Configuration.from(data: data)
     }
 
     @discardableResult
     public static func write(configuration: Configuration) throws -> Configuration {
-        try FileManager.default.createDirectory(at: Paths.configurationRoot, withIntermediateDirectories: true)
-        let encoder = JSONEncoder()
-        encoder.outputFormatting = .prettyPrinted
-        let data = try encoder.encode(configuration)
+        try createConfigurationDirectoryIfNeeded()
+
+        let data = try jsonEncoder.encode(configuration)
         try data.write(to: Paths.configurationFilePath)
+
         return configuration
     }
+
+    private static let jsonEncoder: JSONEncoder = {
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+        return encoder
+    }()
 }
