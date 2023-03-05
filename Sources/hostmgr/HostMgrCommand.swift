c12cadb46798d75c230fd4d5fd789a86634053ab
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2023-03-05 16:15:53 -0700

    Tag 0.20.0-rc-2

--- Sources/hostmgr/HostMgrCommand.swift
+++ Sources/hostmgr/HostMgrCommand.swift
@@ -5,7 +5,7 @@ import libhostmgr
 @main
 struct Hostmgr: AsyncParsableCommand {
 
-    private static var appVersion = "0.20.0-rc-1"
+    private static var appVersion = "0.20.0-rc-2"
 
     static var configuration = CommandConfiguration(
         abstract: "A utility for managing VM hosts",
