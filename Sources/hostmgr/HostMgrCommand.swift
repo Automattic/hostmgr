aaf74f4ce6c4b86689edf243e3b943b6eceee3c6
Tony Li <tony.li@automattic.com>
2023-09-28 10:20:17 -0700

    Release 0.17.3

--- Sources/hostmgr/HostMgrCommand.swift
+++ Sources/hostmgr/HostMgrCommand.swift
@@ -5,7 +5,7 @@ import libhostmgr
 @main
 struct Hostmgr: AsyncParsableCommand {
 
-    private static var appVersion = "0.17.2"
+    private static var appVersion = "0.17.3"
 
     static var configuration = CommandConfiguration(
         abstract: "A utility for managing VM hosts",
