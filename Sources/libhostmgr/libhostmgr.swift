17e9cd228d1db3536d3fb18baee5f3a2f84aa91e
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2023-11-17 22:35:49 -0700

    Cut 0.50.0-beta.7

--- Sources/libhostmgr/libhostmgr.swift
+++ Sources/libhostmgr/libhostmgr.swift
@@ -1,7 +1,7 @@
 import Foundation
 import OSLog
 
-public let hostmgrVersion = "0.50.0-beta.6"
+public let hostmgrVersion = "0.50.0-beta.7"
 
 public extension Logger {
     static let lib    = Logger(subsystem: "com.automattic.hostmgr", category: "lib")
