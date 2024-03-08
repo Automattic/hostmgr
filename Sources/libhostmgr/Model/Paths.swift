54eb1a090a150c1f5306e5d0764d8c8d8de09255
Olivier Halligon <olivier.halligon@automattic.com>
2024-03-08 13:19:13 +0100

    Move `public` declaration to `extension`
    To avoid repeating it on each individual declaration, which were already all `public`

--- Sources/libhostmgr/Model/Paths.swift
+++ Sources/libhostmgr/Model/Paths.swift
@@ -22,46 +22,49 @@ public struct Paths {
             .appendingPathComponent("hostmgr", isDirectory: true)
             .appendingPathComponent("state", isDirectory: true)
     }()
-    
-    public static let vmUsageFile = stateRoot.appendingPathComponent("vm-usage")
+}
 
-    public static let vmImageStorageDirectory: URL = {
+// Public Paths to hostmgr-specific directories
+public extension Paths {
+    static let vmUsageFile = stateRoot.appendingPathComponent("vm-usage")
+
+    static let vmImageStorageDirectory: URL = {
         storageRoot.appendingPathComponent("vm-images", isDirectory: true)
     }()
 
-    public static let ephemeralVMStorageDirectory: URL = {
+    static let ephemeralVMStorageDirectory: URL = {
         tempDirectory.appendingPathComponent("virtual-machines", isDirectory: true)
     }()
 
-    public static let vmWorkingStorageDirectory: URL = {
+    static let vmWorkingStorageDirectory: URL = {
         storageRoot.appendingPathComponent("working-vm-images")
     }()
 
-    public static let gitMirrorStorageDirectory: URL = {
+    static let gitMirrorStorageDirectory: URL = {
         storageRoot.appendingPathComponent("git-mirrors", isDirectory: true)
     }()
 
-    public static let restoreImageDirectory: URL = {
+    static let restoreImageDirectory: URL = {
         storageRoot.appendingPathComponent("restore-images", isDirectory: true)
     }()
 }
 
-// Other Paths outside `/opt/ci`
-extension Paths {
-    public static let authorizedKeysFilePath: URL = {
+// System Paths outside `/opt/ci`
+public extension Paths {
+    static let authorizedKeysFilePath: URL = {
         FileManager.default
             .homeDirectoryForCurrentUser
             .appendingPathComponent(".ssh")
             .appendingPathComponent("authorized_keys")
     }()
 
-    public static let userLaunchAgentsDirectory: URL = {
+    static let userLaunchAgentsDirectory: URL = {
         FileManager.default.urls(for: .libraryDirectory, in: .userDomainMask)
             .first!
             .appendingPathComponent("LaunchAgents")
     }()
 
-    public static let logsDirectory: URL = {
+    static let logsDirectory: URL = {
         FileManager.default.urls(for: .libraryDirectory, in: .userDomainMask)
             .first!
             .appendingPathComponent("Logs")
@@ -70,46 +73,46 @@ extension Paths {
 }
 
 // Functions to get Paths to a specific VM name or repo
-extension Paths {
-    public static func toAppleSiliconVM(named name: String) -> URL {
+public extension Paths {
+    static func toAppleSiliconVM(named name: String) -> URL {
         Paths.vmImageStorageDirectory.appendingPathComponent(name).appendingPathExtension("bundle")
     }
 
-    public static func toWorkingAppleSiliconVM(named name: String) -> URL {
+    static func toWorkingAppleSiliconVM(named name: String) -> URL {
         Paths.vmWorkingStorageDirectory.appendingPathComponent(name).appendingPathExtension("bundle")
     }
 
-    public static func toWorkingParallelsVM(named name: String) -> URL {
+    static func toWorkingParallelsVM(named name: String) -> URL {
         Paths.vmWorkingStorageDirectory.appendingPathComponent(name).appendingPathExtension("pvm")
     }
 
-    public static func toVMTemplate(named name: String) -> URL {
+    static func toVMTemplate(named name: String) -> URL {
         Paths.vmImageStorageDirectory.appendingPathComponent(name).appendingPathExtension("vmtemplate")
     }
 
-    public static func toArchivedVM(named name: String) -> URL {
+    static func toArchivedVM(named name: String) -> URL {
         Paths.vmImageStorageDirectory
             .appendingPathComponent(name)
             .appendingPathExtension("vmtemplate")
             .appendingPathExtension("aar")
     }
 
-    public static func toGitMirror(atURL url: URL) -> URL {
+    static func toGitMirror(atURL url: URL) -> URL {
         gitMirrorStorageDirectory.appendingPathComponent(url.absoluteString.slugify())
     }
 }
 
 // Paths needed by Buildkite's own config
-extension Paths {
-    public static let buildkiteBuildDirectory: URL = {
+public extension Paths {
+    static let buildkiteBuildDirectory: URL = {
         storageRoot.appendingPathComponent("builds")
     }()
 
-    public static let buildkiteHooksDirectory: URL = {
+    static let buildkiteHooksDirectory: URL = {
         storageRoot.appendingPathComponent("hooks")
     }()
 
-    public static let buildkitePluginsDirectory: URL = {
+    static let buildkitePluginsDirectory: URL = {
         storageRoot.appendingPathComponent("plugins")
     }()
 }
