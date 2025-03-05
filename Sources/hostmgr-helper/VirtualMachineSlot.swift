9322b629efc51b43f2a512d561710085034e09e6
Tanner W. Stokes <tanner.stokes@automattic.com>
2025-03-04 20:13:46 -0500

    Separate the ManagedVirtualMachine lifecycle from VirtualMachineSlot.

--- Sources/hostmgr-helper/VirtualMachineSlot.swift
+++ Sources/hostmgr-helper/VirtualMachineSlot.swift
@@ -19,21 +19,18 @@ class VirtualMachineSlot: NSObject, ObservableObject {
 
     enum Status: Sendable {
         case empty
-        case starting(LaunchConfiguration)
-        case running(LaunchConfiguration, IPv4Address)
-        case stopping
+        case starting(Task<Void, Error>, ManagedVirtualMachine)
+        case running(ManagedVirtualMachine)
+        case stopping(ManagedVirtualMachine)
         case crashed(Error)
     }
 
-    struct ManagedVirtualMachine {
-        let machine: VZVirtualMachine
-        let config: LaunchConfiguration
+    enum Errors: Error {
+        /// the slot was asked to start a new VM when it wasn't available
+        case invalidStartState
+        /// the VM was stopped while starting
+        case vmStartCancelled
     }
-
-    private let vmManager = VMManager()
-
-    @Published
-    var managedVirtualMachine: ManagedVirtualMachine?
 
     @Published @MainActor
     var status: Status = .empty
@@ -47,35 +44,23 @@ class VirtualMachineSlot: NSObject, ObservableObject {
 
     @MainActor
     func start(launchConfiguration: LaunchConfiguration) async throws {
-
-        self.status = .starting(launchConfiguration)
+        guard isAvailable else {
+            throw Errors.invalidStartState
+        }
 
         do {
-            let virtualMachine = try await launchConfiguration.setupVirtualMachine()
-            virtualMachine.delegate = self
-            self.managedVirtualMachine = ManagedVirtualMachine(machine: virtualMachine, config: launchConfiguration)
+            let mvm = ManagedVirtualMachine(config: launchConfiguration)
+            let startTask = mvm.start()
+            self.status = .starting(startTask, mvm)
 
-            try await virtualMachine.start()
-
-            if launchConfiguration.waitForNetworking {
-                let ipAddress = try await vmManager.ipAddress(forVmWithName: launchConfiguration.handle)
-                Logger.helper.log("Startup complete – IP Address: \(ipAddress.debugDescription)")
-                self.status = .running(launchConfiguration, ipAddress)
-            } else {
-                Logger.helper.log("Startup in progress – skipped waiting for IP address per launch configuration")
-                self.status = .running(launchConfiguration, .any)
+            try await startTask.value
+            if startTask.isCancelled {
+                throw Errors.vmStartCancelled
             }
+            mvm.machine?.delegate = self
+            self.status = .running(mvm)
         } catch {
-            Logger.helper.error("Error launching VM: \(error.localizedDescription)")
-            Logger.helper.error("Attempting Cleanup of \(launchConfiguration.handle)")
-
-            do {
-                try await vmManager.removeVM(name: launchConfiguration.handle)
-            } catch {
-                Logger.helper.error("Failed to remove VM file: \(error)")
-            }
-            self.managedVirtualMachine = nil
-            self.status = .crashed(error)
+            try? await stop(withError: error)
             throw error
         }
     }
@@ -83,60 +68,48 @@ class VirtualMachineSlot: NSObject, ObservableObject {
     @MainActor
     func stop(withError error: Error? = nil) async throws {
         switch status {
-        case .starting, .running:
-            self.status = .stopping
-        default:
-            break
-        }
-        if let machine = managedVirtualMachine?.machine, machine.canStop {
-            do {
-                try await machine.stop()
-            } catch {
-                Logger.helper.error("Failed to stop VM: \(error)")
-            }
-        }
-        await resetSlot()
-    }
-
-    /// Resets the slot to a stopped status
-    ///
-    /// No need to do anything except some internal bookkeeping
-    /// - Parameter error: Error supplied if the VM crashed
-    @MainActor
-    func resetSlot(withError error: Error? = nil) async {
-        if let managedVirtualMachine {
-            do {
-                try await vmManager.removeVM(name: managedVirtualMachine.config.handle)
-            } catch {
-                Logger.helper.error("Failed to remove VM file: \(error)")
-            }
-            self.managedVirtualMachine = nil
+        case .starting(let task, let mvm):
+            status = .stopping(mvm)
+            task.cancel()
+            await mvm.stop()
+        case .running(let mvm):
+            status = .stopping(mvm)
+            await mvm.stop()
+        case .stopping, .empty, .crashed:
+            Logger.helper.debug("\(self.role.displayName) slot was asked to stop with \(status) status.")
+            return
         }
 
         if let error {
-            Logger.helper.error("Resetting slot with crashed state: \(error)")
+            Logger.helper.error("Resetting \(self.role) slot with crashed state: \(error)")
             self.status = .crashed(error)
         } else {
+            Logger.helper.debug("Setting \(self.role) slot to empty state.")
             self.status = .empty
         }
     }
 
     @MainActor
     func isConfiguredForHandle(_ handle: String) -> Bool {
-        guard let configHandle = managedVirtualMachine?.config.handle else {
+        switch status {
+        case .starting(_, let mvm), .running(let mvm), .stopping(let mvm):
+            Logger.helper.debug(
+                "Comparing \(mvm.handle) and \(handle)"
+            )
+            return mvm.handle == handle
+        case .empty, .crashed:
+            Logger.helper.debug("\(self.role) slot had no handle configured")
             return false
         }
-        Logger.helper.debug(
-            "Comparing \(configHandle) and \(handle)"
-        )
-        return configHandle == handle
     }
 
     @MainActor
     var isAvailable: Bool {
         switch self.status {
-        case .empty, .crashed: return true
-        default: return false
+        case .starting, .running, .stopping:
+            return false
+        case .empty, .crashed:
+            return true
         }
     }
 }
